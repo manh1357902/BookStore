03235ef12abad3a7f4f8b40f7fcb87a6de6ec2d3
manh1357902 <manh7a2002@gmail.com>
2024-06-03 16:49:14 +0700

    init project

--- src/main/java/come/example/bookstore/entities/User.java
+++ src/main/java/come/example/bookstore/entities/User.java
@@ -1,4 +1,15 @@
 package come.example.bookstore.entities;
 
-public class User {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Data
+@Table(name = "users")
+public class User extends BaseEntity{
+
 }
